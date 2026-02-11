52fd595d7c9344d7597efd0d92c91d37ea0fced9
Numan2005 <numanyahya258@gmail.com>
2026-02-11 14:43:11 +0530

    problems

---
+++ SlidingProb17.java
@@ -0,0 +1,39 @@
+package com.sliding;
+//skip negative numbers / find maximum sub array
+public class SlidingProb17 {
+	public static void minNeg(int[] arr,int k) {
+		int i =0,j=0;
+		int sum =0;
+		int min = Integer.MAX_VALUE;
+		int startIndex =0;
+		while(i<arr.length){
+			if(arr[i]>0) {
+			sum+=arr[i];
+			}
+			if(i-j+1<k) {
+				i++;
+			}else { 
+				if(sum<min) {
+					min = sum;
+					startIndex =j;
+				}
+				if(arr[j]>0) {
+				sum-=arr[j];
+				}
+				i++;
+				j++;
+			}
+		}
+		System.out.println(min);
+		for(int x=startIndex;x<startIndex+k;x++) {
+			System.out.print(arr[x]+" ");
+		}
+	}
+	public static void main(String[] args) {
+		 int[] arr = {-4,-7,-11,-9,-12};
+		  int k = 3;
+		  minNeg(arr,k);
+
+	}
+
+}
